96a0ada8d76e261b82ff1df96e972c6a833e42cd
Jordan George <jordan.geo777@gmail.com>
2020-11-02 21:42:09 +0000

    Updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51Hj7AHKJ1dQPNcRmw3iZF9CdY1ZXo0IDgqyTIp8MOsYOWYUAm9VaaMken9iM9Q6fszP3P4pT6yhmwN42QwccsDQm008czzB0aj';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Crown Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/Cuz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
